97aa1f5ec6ce98a682556c4389ff1b0d1b54f462
pepoloco <banchevpepo@gmail.com>
2023-03-21 22:11:42 +0200

    no message

--- oop_stopWatch.js
+++ oop_stopWatch.js
@@ -1,2 +1,36 @@
-let x = 0
-console.log(x)
+// function Stopwatch() {
+//   let startTime,
+//     endTime,
+//     running,
+//     duration = 0;
+
+//   this.start = function () {
+//     if (running) throw new Error("Stopwatch has already started.");
+//     running = true;
+//     startTime = new Date();
+//   };
+//   this.stop = function () {
+//     if (!running) throw new Error(`Stopwatch is not started.`);
+//     running = false;
+//     endTime = new Date();
+//     const secs = (endTime.getTime() - startTime.getTimE()) / 1000;
+//     duration += secs;
+//   };
+//   this.reset = function () {
+//     startTime = null;
+//     endTime = null;
+//     running = false;
+//     duration = 0;
+//   };
+// }
+
+// Stopwatch();
+
+// object StopWatch
+// duration -> reset , start , stop
+//
+//
+const circle = {
+  radius: 1,
+  draw: function () {},
+};
