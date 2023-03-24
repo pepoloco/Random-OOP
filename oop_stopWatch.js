34ac671a02ac61e315e73d7b72af29547b8350f6
pepoloco <banchevpepo@gmail.com>
2023-03-24 22:14:01 +0200

    no message

--- oop_stopWatch.js
+++ oop_stopWatch.js
@@ -1,36 +1,33 @@
-// function Stopwatch() {
-//   let startTime,
-//     endTime,
-//     running,
-//     duration = 0;
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-//   this.start = function () {
-//     if (running) throw new Error("Stopwatch has already started.");
-//     running = true;
-//     startTime = new Date();
-//   };
-//   this.stop = function () {
-//     if (!running) throw new Error(`Stopwatch is not started.`);
-//     running = false;
-//     endTime = new Date();
-//     const secs = (endTime.getTime() - startTime.getTimE()) / 1000;
-//     duration += secs;
-//   };
-//   this.reset = function () {
-//     startTime = null;
-//     endTime = null;
-//     running = false;
-//     duration = 0;
-//   };
-// }
-
-// Stopwatch();
+  this.start = function () {
+    if (running) console.log("Stopwatch has already started.");
+    running = true;
+    startTime = new Date();
+  };
+  this.stop = function () {
+    if (!running) console.log(`Stopwatch is not started.`);
+    running = false;
+    endTime = new Date();
+    const secs = (endTime.getTime() - startTime.getTimE()) / 1000;
+    duration += secs;
+  };
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
+}
 
 // object StopWatch
 // duration -> reset , start , stop
-//
-//
-const circle = {
-  radius: 1,
-  draw: function () {},
-};
